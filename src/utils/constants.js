c47b33dd3a045d4f97c3c6686ccdcd2e96a8fba5
Амаль <50665935+WTTeneger@users.noreply.github.com>
2024-07-31 03:53:40 +0300

    sda

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 // const API_BASE_URL = 'http://localhost:3030/api';
-const API_BASE_URL = 'http://localhost:3030/api';
-// const API_BASE_URL = 'https://api.mellstroycoin.tech/api';
+// const API_BASE_URL = 'http://localhost:3030/api';
+const API_BASE_URL = 'https://api.mellstroycoin.tech/api';
 
 export const MAX_ENERGY = 5000;
 
