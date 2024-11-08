8dd05aa89a1c93cdd903908eb86ba9fa3b3b30a0
Амаль <50665935+WTTeneger@users.noreply.github.com>
2024-11-08 21:36:46 +0300

    new build

--- dist/assets/index-D93LP0F0.js
+++ dist/assets/index-D93LP0F0.js
@@ -1,4 +1,4 @@
-import{n as fe,s as Ke,T as k,t as P,a as g,o as Y,R as Dr,p as H,y as ae}from"./index-BcA0iQMJ.js";import{bj as Ur}from"./index-21Bkhl7p.js";/**
+import{n as fe,s as Ke,T as k,t as P,a as g,o as Y,R as Dr,p as H,y as ae}from"./index-CypmoC07.js";import{bj as Ur}from"./index-Dfgct4bJ.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
