c90320bf2102a9197ec91e8a0b24e443b0e2477e
Амаль <50665935+WTTeneger@users.noreply.github.com>
2024-11-10 23:58:01 +0300

    new build

--- dist/assets/index-CP1b1n0E.js
+++ dist/assets/index-CP1b1n0E.js
@@ -1,4 +1,4 @@
-import{n as fe,s as Ke,T as k,t as P,a as g,o as Y,R as Dr,p as H,y as ae}from"./index-Cn74CDpW.js";import{bj as Ur}from"./index-cwzQL5_G.js";/**
+import{n as fe,s as Ke,T as k,t as P,a as g,o as Y,R as Dr,p as H,y as ae}from"./index-B1zHdMtc.js";import{bj as Ur}from"./index-g_l8TpsF.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
