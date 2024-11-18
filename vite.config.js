9598976de5ee72a6711e8657fb71d08591023d4b
Амаль <50665935+WTTeneger@users.noreply.github.com>
2024-11-18 03:47:21 +0300

    nd

--- vite.config.js
+++ vite.config.js
@@ -20,6 +20,6 @@ import mkcert from 'vite-plugin-mkcert';
 export default defineConfig({
   server: { https: true }, // Not needed for Vite 5+
   plugins: [mkcert(), react()],
-  build: { chunkSizeWarningLimit: 1600 },
+  build: { chunkSizeWarningLimit: 25600 },
 });
 
