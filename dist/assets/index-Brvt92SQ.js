41b8b32d33fe5559f1cbec8929561382adb0e09a
Амаль <50665935+WTTeneger@users.noreply.github.com>
2024-11-18 18:35:08 +0300

    new

--- dist/assets/index-Brvt92SQ.js
+++ dist/assets/index-Brvt92SQ.js
@@ -1,4 +1,4 @@
-import{n as fe,s as Ke,T as k,t as P,a as g,o as Y,R as Dr,p as H,y as ae}from"./index-D3h7cXg2.js";import{bj as Ur}from"./index-Cfot6o4M.js";/**
+import{n as fe,s as Ke,T as k,t as P,a as g,o as Y,R as Dr,p as H,y as ae}from"./index-CpsKiuLg.js";import{bj as Ur}from"./index-BdPcWwFb.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
