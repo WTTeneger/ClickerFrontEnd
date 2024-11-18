6c7a6416d643f379bd485b668675942f10c5110f
Амаль <50665935+WTTeneger@users.noreply.github.com>
2024-11-18 04:28:04 +0300

    dasd

--- dist/assets/index-0V2e2bgQ.js
+++ dist/assets/index-0V2e2bgQ.js
@@ -1,4 +1,4 @@
-import{n as fe,s as Ke,T as k,t as P,a as g,o as Y,R as Dr,p as H,y as ae}from"./index-DZw534RT.js";import{bj as Ur}from"./index-DotAZKz9.js";/**
+import{n as fe,s as Ke,T as k,t as P,a as g,o as Y,R as Dr,p as H,y as ae}from"./index-DA-VoaFk.js";import{bj as Ur}from"./index-Cloax-fm.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
--- src/components/HeaderBar/HeaderBar.jsx
+++ src/components/HeaderBar/HeaderBar.jsx
@@ -17,6 +17,7 @@ const HeaderBar = ({ Click, custom = false }) => {
   if (inter.header == false && custom == false) return null;
 
   let isFullScreen = window?.Telegram.WebApp?.isFullscreen || false;
+  isFullScreen = true;
   return (
     <div className={s['header']} id='BASE_HEADER'>
       <div className={`${s['base']} ${isFullScreen ? s['full-screen'] : ''}`}>
